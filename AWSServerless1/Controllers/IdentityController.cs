dd91fb861c585be8e405c85de43c356145357bd0
Seyma Alpak <seyma.alpak@patikaglobal.com>
2022-12-07 16:05:31 +0300

    okta removed from controller

--- AWSServerless1/Controllers/IdentityController.cs
+++ AWSServerless1/Controllers/IdentityController.cs
@@ -19,38 +19,5 @@ namespace AWSServerless1.Controllers
             UserStore = serviceProvider.GetService<IUserStore<ApplicationUser>>();
             SignInManager = serviceProvider.GetService<SignInManager<ApplicationUser>>();
         }
-
-        [HttpGet("deneme")]
-        public IActionResult Deneme()
-        {
-            return Ok("okk");
-        }
-
-        [HttpGet("okta")]
-        public IActionResult LoginWithOktaAsync()
-        {
-            var callback = "identity/deneme";
-            var provider = OpenIdConnectDefaults.AuthenticationScheme;
-            var url = $"/identity/authorize/callback?callback={callback}";
-
-            if (string.IsNullOrEmpty(callback))
-            {
-                throw new Exception("Callback is empty");
-            }
-            var properties = SignInManager.ConfigureExternalAuthenticationProperties(provider, url);
-            return new ChallengeResult(provider, properties);
-        }
-
-        [HttpGet("authorize/callback")]
-        public async Task<IActionResult> RegisterCallbackAsync(string callback)
-        {
-            var info = await SignInManager.GetExternalLoginInfoAsync();
-            var result = await SignInManager.ExternalLoginSignInAsync(info.LoginProvider, info.ProviderKey, isPersistent: false, bypassTwoFactor: true);
-            
-            if (result.Succeeded)
-                return Ok("ok");
-            else
-                return BadRequest("bad");
-        }
     }
 }
