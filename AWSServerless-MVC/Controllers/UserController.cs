550886d7c7d4b034825ce1c8b97a18574e024254
Şeyma Alpak <seyma.alpak@patikaglobal.com>
2022-12-16 13:17:12 +0300

    error endpoint removed

--- AWSServerless-MVC/Controllers/UserController.cs
+++ AWSServerless-MVC/Controllers/UserController.cs
@@ -14,11 +14,6 @@ namespace AWSServerless_MVC.Controllers
         {
             Repo = repo;
         }
-        [HttpGet("error")]
-        public IActionResult GetError()
-        {
-            return BadRequest("Jitterbit Esra");
-        }
         [HttpGet("{userId}")]
         public async Task<IActionResult> GetById(Guid userId)
         {
