df41d490d4374a68a20b29db1bfd691be8c8ade3
Şeyma Alpak <seyma.alpak@patikaglobal.com>
2022-12-26 11:19:43 +0300

    File upload done with framework

---
+++ AWSServerless-MVC/Controllers/FileController.cs
@@ -0,0 +1,26 @@
+﻿using AWSServerless_MVC.Interfaces.Repositories;
+using AWSServerless_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Patika.Framework.Utilities.Excel.Extensions;
+
+namespace AWSServerless_MVC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        private readonly IUserRepository Repo;
+        public FileController(IUserRepository repo)
+        {
+            Repo = repo;
+        }
+        [HttpPost]
+        [Consumes("multipart/form-data")]
+        [Produces("application/json")]
+        public async Task<IActionResult> Createuser(IFormFile file)
+        {
+            var data = file.MapExcelToEnumarable<ExampleExcel>();
+            return Ok(data);
+        }
+    }
+}
---
+++ AWSServerless-MVC/Models/ExampleExcel.cs
@@ -0,0 +1,14 @@
+﻿using Npoi.Mapper.Attributes;
+using Patika.Framework.Utilities.Excel.Interfaces;
+
+namespace AWSServerless_MVC.Models
+{
+    public class ExampleExcel : IExportable
+    {
+        [Column("Ad")]
+        public string Name { get; set; } = string.Empty;
+
+        [Column("Yaş")]
+        public int Age { get; set; }
+    }
+}
